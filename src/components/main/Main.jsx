a694e24dfff901821d0cf8fe6689d1d7ae3d3185
Ramon Mauricio <ArgoTenacius@gmail.com>
2022-06-11 17:49:02 -0300

    Header of sidebar and search bar added.

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,12 +1,36 @@
 import React from 'react'
 import './main.css'
+import {BsFillChatLeftTextFill} from 'react-icons/bs'
+import { TextInput, TextInputProps, ActionIcon, useMantineTheme } from '@mantine/core';
+import { Search, ArrowRight, ArrowLeft } from 'tabler-icons-react';
 
 const Main = ({user}) => {
-    return (
-        <main>
-            <header>
+    const theme = useMantineTheme();
 
-            </header>
+    return (
+        <main className='main'>
+            <div className='main__sidebar'>
+                <header className='main__sidebar-user'>
+                    <img src='https://excellence.truman.edu/files/2022/02/Photo-Placeholder-Image-150x150-1.jpg' alt='profile_photo'/>
+                    <div>
+                        <BsFillChatLeftTextFill className='app-icon'/>
+                    </div>
+                </header>
+
+                <TextInput
+                    icon={<Search size={18} />}
+                    radius="xl"
+                    style={{marginBlock: "1rem"}}
+                    size="md"
+                    rightSection={
+                        <ActionIcon size={32} style={{cursor: "pointer"}} radius="xl" color={theme.primaryColor} variant="filled">
+                        {theme.dir === 'ltr' ? <ArrowRight size={18} /> : <ArrowLeft size={18} />}
+                        </ActionIcon>
+                    }
+                    placeholder="Search someone"
+                    rightSectionWidth={42}
+                />
+            </div>
         </main>
     )
 }
